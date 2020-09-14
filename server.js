0957cccc58d3d63e384d68fd25ae3f561c762b28
nwheatle <wheatley.nm@gmail.com>
2020-09-14 14:53:04 -0700

    second commit

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const port = 3001;
 
 const app = express();
+
 app.use(
-  "/coins/markets",
+  "/api_to_external_website",
   createProxyMiddleware({
     target:
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false",
@@ -15,6 +16,25 @@ app.use(
     changeOrigin: true,
   })
 );
+
+app.use(
+  "/localhostapi",
+  createProxyMiddleware({
+    target: `http://localhost:${port}/`,
+    headers: {
+      accept: "application/json",
+      method: "GET",
+    },
+    changeOrigin: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  console.log("localhost:3001 api is running");
+  const data = { result: `Success! from localhostapi on localhost:${port}!!` };
+  res.send(JSON.parse(data));
+});
+
 app.listen(port, function () {
   console.log(`server running now.. ${port}`);
 });
--- src/App.js
+++ src/App.js
@@ -2,20 +2,60 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [res, setRes] = useState("initial state");
+  const [externalServerResponse, setExternalServerResponse] = useState(
+    "no response"
+  );
+  const [localhostServerResponse, setLocalhostServerResponse] = useState(
+    "no response"
+  );
 
-  const getAPI = async () => {
-    const result = await fetch("http://localhost:3001/coins/markets");
+  const getFirstAPI = async () => {
+    console.log("calling external api from client");
+    const result = await fetch("http://localhost:3001/api_to_external_website");
     console.log("result", result);
     const data = await result.json();
     console.log("data", data);
-    setRes(JSON.stringify(data));
+    setExternalServerResponse(JSON.stringify(data[0]));
   };
+
+  const getSecondAPI = async () => {
+    console.log("calling localhost api from client");
+    const result = await fetch("/localhostapi/users");
+    console.log("result", result);
+    const data = await result.json();
+    console.log("data", data);
+    setLocalhostServerResponse(JSON.stringify(data));
+  };
+
   useEffect(() => {
-    getAPI();
+    getFirstAPI();
+    getSecondAPI();
   }, []);
 
-  return <div className="App">{res}</div>;
+  return (
+    <div className="App">
+      <div style={{ marginTop: "3em", marginBottom: "1em" }}>
+        <h2>
+          Response from{" "}
+          <code>
+            <i>www.api.coingecko.com/api</i>
+          </code>
+          :
+        </h2>
+      </div>
+      <div>{externalServerResponse}</div>
+      <div style={{ marginTop: "3em", marginBottom: "1em" }}>
+        <h2>
+          Response from{" "}
+          <code>
+            <i>localhost:3001</i>
+          </code>{" "}
+          :{" "}
+        </h2>
+      </div>
+      <div>{localhostServerResponse}</div>
+    </div>
+  );
 }
 
 export default App;
